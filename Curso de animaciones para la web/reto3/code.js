64cd27089eb3770c094f5c885cee0a4f49914f3c
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2021-03-12 22:18:05 -0600

    reto 3 de curso de animaciones para la web

--- Curso de animaciones para la web/reto3/code.js
+++ Curso de animaciones para la web/reto3/code.js
@@ -20,16 +20,40 @@ const reverseButton = document.getElementById('reverse');
 
 playButton.addEventListener('click', () => {
     animation.play();
+    playButton.classList.toggle('activo')
+    if (pauseButton.classList.contains('activo') || stopButton.classList.contains('activo') || reverseButton.classList.contains('activo')) {
+        pauseButton.classList.remove('activo')
+        stopButton.classList.remove('activo')
+        reverseButton.classList.remove('activo')
+    }
 });
 
 pauseButton.addEventListener('click', () => {
     animation.pause();
+    pauseButton.classList.toggle('activo')
+    if (playButton.classList.contains('activo') || stopButton.classList.contains('activo') || reverseButton.classList.contains('activo')) {
+        playButton.classList.remove('activo')
+        stopButton.classList.remove('activo')
+        reverseButton.classList.remove('activo')
+    }
 });
 
 stopButton.addEventListener('click', () => {
     animation.cancel();
+    stopButton.classList.toggle('activo')
+    if (playButton.classList.contains('activo') || pauseButton.classList.contains('activo') || reverseButton.classList.contains('activo')) {
+        playButton.classList.remove('activo')
+        pauseButton.classList.remove('activo')
+        reverseButton.classList.remove('activo')
+    }
 });
 
 reverseButton.addEventListener('click', () => {
     animation.reverse();
+    reverseButton.classList.toggle('activo')
+    if (playButton.classList.contains('activo') || pauseButton.classList.contains('activo') || stopButton.classList.contains('activo')) {
+        playButton.classList.remove('activo')
+        pauseButton.classList.remove('activo')
+        stopButton.classList.remove('activo')
+    }
 });
